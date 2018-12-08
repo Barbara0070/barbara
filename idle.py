4ae94ca3bfd8b5fef36a8c1848eb9fb88f29e49d
Barbara <w2ertygf21@gmail.com>
2018-12-08 14:22:25 +0100

    'initial'

---
+++ idle.py
@@ -0,0 +1,14 @@
+print(1==1)
+#a=10 h=5 trojkat
+#kolo r=7
+#trapez a=3 b=9 h=5
+#objetosc kuli o zadanym promieniu
+a=10*5/2
+print("pole trojkata wynosi " + str(a))
+b=3.14*7*7
+print("pole kola wynosi " + str(b))
+c=(3+9)*5/2
+print("pole trapezu wynosi " + str(c))
+r=1
+d=4/3*3.14*r**3
+print("pole kuli o promieniu ", r, "wynosi", d)
